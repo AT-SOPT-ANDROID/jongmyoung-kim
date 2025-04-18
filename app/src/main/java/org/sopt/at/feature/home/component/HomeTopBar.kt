d619d78192842ba5bd92078af98673b909e3d147
Jong <jm991014@gmail.com>
2025-04-19 02:57:18 +0900

    [ui/#3] home top bar 구현

---
+++ app/src/main/java/org/sopt/at/feature/home/component/HomeTopBar.kt
@@ -0,0 +1,69 @@
+package org.sopt.at.feature.home.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import org.sopt.at.R.drawable.ic_cast
+import org.sopt.at.R.drawable.img_profile_placeholder
+import org.sopt.at.R.drawable.logo_tving
+import org.sopt.at.core.designsystem.common.AtSoptDefaultTopBar
+import org.sopt.at.core.designsystem.theme.ATSOPTANDROIDTheme
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun HomeTopBar(
+    profileImageUrl: String,
+    modifier: Modifier = Modifier,
+) {
+    AtSoptDefaultTopBar(
+        modifier = modifier.fillMaxWidth(),
+        leadingIcon = {
+            Icon(
+                imageVector = ImageVector.vectorResource(logo_tving),
+                contentDescription = null,
+                tint = Color.Unspecified,
+            )
+        },
+        trailingIcon = {
+            Row(
+                modifier = Modifier,
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(ic_cast),
+                    contentDescription = null,
+                    tint = Color.Unspecified,
+                )
+                GlideImage(
+                    model = profileImageUrl,
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp),
+                ) {
+                    it.placeholder(img_profile_placeholder)
+                }
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun HomeTopBarPreview() {
+    ATSOPTANDROIDTheme {
+        HomeTopBar(
+            profileImageUrl = "",
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
