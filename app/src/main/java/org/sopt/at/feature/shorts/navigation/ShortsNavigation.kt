9440b119427a3acb57912eb3be4f7783e8952152
Jong <jm991014@gmail.com>
2025-04-19 04:24:24 +0900

    [ui/#3] Shorts 화면 약식 구현

--- app/src/main/java/org/sopt/at/feature/shorts/navigation/ShortsNavigation.kt
+++ app/src/main/java/org/sopt/at/feature/shorts/navigation/ShortsNavigation.kt
@@ -7,6 +7,7 @@ import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import kotlinx.serialization.Serializable
 import org.sopt.at.core.common.navigation.MainTabRoute
+import org.sopt.at.feature.shorts.ShortsRoute
 
 fun NavController.navigateToShorts(navOptions: NavOptions? = null) = navigate(Shorts, navOptions)
 
@@ -14,7 +15,9 @@ fun NavGraphBuilder.shortsGraph(
     modifier: Modifier = Modifier,
 ) {
     composable<Shorts> {
-
+        ShortsRoute(
+            modifier = modifier,
+        )
     }
 }
 
