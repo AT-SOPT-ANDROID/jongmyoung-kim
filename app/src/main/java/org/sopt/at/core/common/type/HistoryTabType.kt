c6f1d53be583e6d4b9bb888a7ac97f020a8665e6
Jong <jm991014@gmail.com>
2025-04-23 03:17:11 +0900

    [add/#3] 기록 화면 TabRowType 추가

---
+++ app/src/main/java/org/sopt/at/core/common/type/HistoryTabType.kt
@@ -0,0 +1,10 @@
+package org.sopt.at.core.common.type
+
+enum class HistoryTabType(
+    val title: String,
+) {
+    VIEW_HISTORY("시청내역"),
+    PURCHASE_HISTORY("구매내역"),
+    LIKE_SERIES("찜한 시리즈"),
+    LIKE_MOVIE("찜한 영화"),
+}
