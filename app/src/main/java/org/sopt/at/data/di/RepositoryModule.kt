48ac11dc181e9d38abe7a31a6e07f4fb61c9216d
Jong <jm991014@gmail.com>
2025-04-23 03:16:00 +0900

    [feat/#3] HomeRepository 추상/구현체 및 모듈 구현

---
+++ app/src/main/java/org/sopt/at/data/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package org.sopt.at.data.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.sopt.at.data.repositoryimpl.HomeRepositoryImpl
+import org.sopt.at.domain.repository.HomeRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindHomeRepository(
+        homeRepositoryImpl: HomeRepositoryImpl,
+    ): HomeRepository
+}
