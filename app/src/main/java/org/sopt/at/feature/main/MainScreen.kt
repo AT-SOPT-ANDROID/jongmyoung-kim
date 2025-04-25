d89bf6a849b5523733b1c5dba7a2e79782f77c09
Jong <jm991014@gmail.com>
2025-04-25 17:21:37 +0900

    [add/#3] navGraph 추가, navigate함수 추가

--- app/src/main/java/org/sopt/at/feature/main/MainScreen.kt
+++ app/src/main/java/org/sopt/at/feature/main/MainScreen.kt
@@ -2,20 +2,27 @@ package org.sopt.at.feature.main
 
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import kotlinx.collections.immutable.toImmutableList
+import org.sopt.at.core.designsystem.theme.AtSoptTheme
 import org.sopt.at.feature.history.navigation.historyGraph
 import org.sopt.at.feature.home.navigation.homeGraph
 import org.sopt.at.feature.live.navigation.liveGraph
 import org.sopt.at.feature.main.component.MainBottomBar
+import org.sopt.at.feature.mypage.navigation.myPageGraph
+import org.sopt.at.feature.mypage.navigation.navigateToMyPage
 import org.sopt.at.feature.search.navigation.searchGraph
 import org.sopt.at.feature.shorts.navigation.shortsGraph
+import org.sopt.at.feature.signin.navigation.signInGraph
+import org.sopt.at.feature.signup.navigation.signUpGraph
 
 @Composable
 fun MainScreen(
@@ -30,7 +37,10 @@ fun MainScreen(
                 onTabSelected = navigator::navigate,
             )
         },
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .background(color = AtSoptTheme.colors.black)
+            .systemBarsPadding()
+            .fillMaxSize(),
     ) { innerPadding ->
         Box(modifier = Modifier.fillMaxSize()) {
             MainNavHost(
@@ -55,6 +65,7 @@ private fun MainNavHost(
         startDestination = navigator.startDestination,
     ) {
         homeGraph(
+            navigateToMyPage = navigator.navController::navigateToMyPage,
             modifier = modifier,
         )
 
@@ -71,6 +82,18 @@ private fun MainNavHost(
         )
 
         historyGraph(
+            modifier = modifier,
+        )
+
+        myPageGraph(
+            modifier = modifier,
+        )
+
+        signInGraph(
+            modifier = modifier,
+        )
+
+        signUpGraph(
             modifier = modifier,
         )
     }
